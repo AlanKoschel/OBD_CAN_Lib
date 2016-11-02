4f5d2386707275a052715575816f0917bf6ec291
AlanKoschel <koschel.alan@gmail.com>
2016-11-02 10:47:23 +0100

    erste Implementierung der Klasse

--- src/OBD_CAN_Lib.cpp
+++ src/OBD_CAN_Lib.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+#include "OBD_CAN_Lib.h"
+
 int main() {
   /* Setup your example here, code that should run once
    */
--- src/OBD_CAN_Lib.h
+++ src/OBD_CAN_Lib.h
@@ -8,6 +8,25 @@
 #ifndef OBD_CAN_LIB_H_
 #define OBD_CAN_LIB_H_
 
+#include "mcp_can_dfs.h"
+#define MAX_BYTES_IN_MESSAGE 8
+
+class CAN_MCP
+{
+	private:
+	INT8U mcp_mode;
+	INT8U mcp_chip_select;
+
+	INT32U can_id;
+	INT8U can_rtr;								//Remote Transmission Request
+	INT8U can_dlc;								//data length code
+	INT8U can_data[MAX_BYTES_IN_MESSAGE];		//CAN Nachricht
+	INT8U can_filhit;							//unsicher ob es nötig ist
+
+
+	public:
+
+};
 
 
 
