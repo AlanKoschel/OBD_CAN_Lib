938a88c7272888b75475f62afcf8d0167d4cefdb
AlanKoschel <koschel.alan@gmail.com>
2016-11-02 21:39:32 +0100

    eweiterung der klasse

--- src/OBD_CAN_Lib.h
+++ src/OBD_CAN_Lib.h
@@ -15,16 +15,19 @@ class CAN_MCP
 {
 	private:
 	INT8U mcp_mode;
-	INT8U mcp_chip_select;
+	INT8U mcp_chip_select;						//notwendig??
 
 	INT32U can_id;
+	INT8U can_ext_flg;							//extended flag
 	INT8U can_rtr;								//Remote Transmission Request
 	INT8U can_dlc;								//data length code
 	INT8U can_data[MAX_BYTES_IN_MESSAGE];		//CAN Nachricht
-	INT8U can_filhit;							//unsicher ob es nötig ist
+	INT8U can_filhit;							//notwendig??
 
 
 	public:
+	CAN_MCP();
+	void resetMPC();
 
 };
 
